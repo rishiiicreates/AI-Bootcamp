56b635ea60431a0550d64756c8123f9922f8bb81
Hrishikesh Yadav <rishiicreates@gmail.com>
2026-09-21 21:08:59 +0530

    Add files via upload

---
+++ invertinglist.py
@@ -0,0 +1,12 @@
+def rev_list(arr):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+
+
+data = [int(x) for x in input().split()]
+rev_list(data)
+print(*data)
---
+++ main.py
@@ -0,0 +1,3 @@
+nums = [int(x) for x in input().split()]
+print(*(x for x in nums if x % 2 == 0))
+
